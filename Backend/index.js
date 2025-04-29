70fd430bcb4870351114285291b6a3c192c9273f
KRISH2507 <thekrishdeepsingh@gmail.com>
2025-04-29 16:51:44 +0530

    Post-api

--- Backend/index.js
+++ Backend/index.js
@@ -24,5 +24,5 @@ app.get('/user/:id' , async (req , res) => {
     
 })
 app.listen(PORT , () => {
-    console.log(`the server is running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 })
