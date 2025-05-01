81eab19dc02ecec1742f58ba0889140c20ecba53
KRISH2507 <thekrishdeepsingh@gmail.com>
2025-05-01 09:40:52 +0530

    Put-api

--- Backend/index.js
+++ Backend/index.js
@@ -35,6 +35,17 @@ app.get('/user/:id', async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 });
+app.put('./user/:id' , async(req , res) => {
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id , req.body , {new:true});
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User updated", updatedUser });
+    }catch(err){
+        res.status(500).json({message : "falls under catch" , error: err.message})
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
